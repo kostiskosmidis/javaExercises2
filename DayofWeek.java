53c0b4b9cc4ed1df0b03dff4398f9010f3b04bb5
Kostis Kosmidis <25617322+kostiskosmidis@users.noreply.github.com>
2020-07-08 15:31:37 +0300

    Update DayofWeek.java

--- DayofWeek.java
+++ DayofWeek.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class DayofWeek {
     public static void main(String[] args)
     {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Input number: ");
-        int day = in.nextInt();
-
-        System.out.println(getDayName(day));
+        //Scanner in = new Scanner(System.in);
+        //System.out.print("Input number: ");
+        //int day = in.nextInt();
+        int day = Integer.parseInt(args[0]);
+        //System.out.println(getDayName(day));
     }
 
     // Get the name for the Week
